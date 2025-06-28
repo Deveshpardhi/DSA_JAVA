9a0a25ae94865c147ee6e058784911e6f91f2885
Deveshpardhi <150068044+Deveshpardhi@users.noreply.github.com>
2025-06-29 01:10:21 +0530

    ArrayList

---
+++ ArryList.java
@@ -0,0 +1,9 @@
+
+public class ArryList<T> {
+
+    public void add(int a) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'add'");
+    }
+
+}
